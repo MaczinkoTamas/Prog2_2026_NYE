18ee05881195716be01df4dce5bb8e019f1ec3b1
Maczinkó Tamás <66484381+MaczinkoTamas@users.noreply.github.com>
2026-02-09 12:03:54 +0100

    Add files via upload

---
+++ Elsohet/Feladatok/src/Main.java
@@ -0,0 +1,15 @@
+public class Main{
+public static void main() {
+    //Exercise 2
+System.out.println("Tamas Maczinko");
+System.out.println("WZWLAV");
+System.out.println("The elevator for success does not work, use the stairs");
+    //Exercise 3
+    //Class name: Electronics
+        //Object: Mouse
+        //Color: Dark_grey
+        //Weight: 10 dkg
+        //Is_on: TRUE
+        //Methods: Scroll, light on red, turnON, CursorHOVER
+}
+}
