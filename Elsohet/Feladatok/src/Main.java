0b9009311331ddfd82e8b0f5fafa31df22d1e5b4
Maczinkó Tamás <66484381+MaczinkoTamas@users.noreply.github.com>
2026-02-09 17:02:06 +0100

    Update Main.java

--- Elsohet/Feladatok/src/Main.java
+++ Elsohet/Feladatok/src/Main.java
@@ -5,11 +5,11 @@ System.out.println("Tamas Maczinko");
 System.out.println("WZWLAV");
 System.out.println("The elevator for success does not work, use the stairs");
     //Exercise 3
-    //Class name: Electronics
-        //String Object: "Mouse"
-        //String Color: "Dark_grey"
-        //String Weight: "10_dkg"
-        //Boolean Is_on: TRUE
-        //String Methods[4]: {"Scroll, light on red, turnON, CursorHOVER"}
+    //Class name: Electronics{
+        //String Object: "Mouse";
+        //String Color: "Dark_grey";
+        //String Weight: "10_dkg";
+        //Boolean Is_on: TRUE;
+        //String Methods[4]: {"Scroll, light on red, turnON, CursorHOVER"};}
 }
 }
