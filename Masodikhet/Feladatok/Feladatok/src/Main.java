c1f53cbf782a6ed6a56ac6ef0db0eb04f166db4e
Maczinkó Tamás <66484381+MaczinkoTamas@users.noreply.github.com>
2026-02-16 09:34:18 +0100

    Add files via upload

---
+++ Masodikhet/Feladatok/Feladatok/src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Main{
+    public static void main(){
+        //Feladat 1
+        Scanner f = new Scanner (System.in);
+        System.out.println("Kerem a homersekletet: ");
+        float h= h.nextint();
+        if (h>=0 && h<30) {
+            System.out.println("Atlagos");
+        }elseif (h>=30){
+            System.out.println("Tul meleg")
+        }
+        //Feladat 2
+        Scanner v= new Scanner (System.in);
+        System.out.println("")
+    }
+}
